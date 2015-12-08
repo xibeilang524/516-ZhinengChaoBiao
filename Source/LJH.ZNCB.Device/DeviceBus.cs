8eb16a2eacb68c331cae18d6f3f0ae2cde658c96
ljh198275823 <ljh198275823@163.com>
2015-12-08 16:18:18 +0800

    1

---
+++ Source/LJH.ZNCB.Device/DeviceBus.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading ;
+using LJH.GeneralLibrary;
+
+namespace LJH.ZNCB.Device
+{
+    internal class DeviceBus
+    {
+        #region 构造函数
+        public DeviceBus(byte comport, int baud)
+        {
+            Comport = comport;
+            _Commport = new CommPort(comport, baud);
+        }
+        #endregion
+
+        #region 私有变量
+        private CommPort _Commport = null;
+
+        private object _DataLocker = new object();
+        private List<byte> _RecievedData = new List<byte>();
+        private AutoResetEvent _PacketReceived = new AutoResetEvent(false);
+        private Packet _Response = null;
+        #endregion
+
+        #region 私有方法
+        private void Commport_OnDataArrivedEvent(object sender, byte[] data)
+        {
+            lock (_DataLocker)
+            {
+                _RecievedData.AddRange(data);
+                _Response = Packet.GetPacket(_RecievedData);
+                if (_Response != null) _PacketReceived.Set();
+            }
+        }
+        #endregion
+
+        #region 公共属性
+        public byte Comport { get; set; }
+        /// <summary>
+        /// 获取读卡器是否已经打开
+        /// </summary>
+        public bool IsOpened
+        {
+            get { return _Commport.PortOpened; }
+        }
+        #endregion
+
+        #region 公共方法
+        /// <summary>
+        /// 打开
+        /// </summary>
+        public void Open()
+        {
+            if (_Commport.PortOpened) _Commport.Close();
+            _Commport.Open();
+            if (_Commport.PortOpened)
+            {
+                _Commport.OnDataArrivedEvent -= new DataArrivedDelegate(Commport_OnDataArrivedEvent);
+                _Commport.OnDataArrivedEvent += new DataArrivedDelegate(Commport_OnDataArrivedEvent);
+            }
+            else
+            {
+                throw new Exception("串口打开失败");
+            }
+        }
+        /// <summary>
+        /// 关闭
+        /// </summary>
+        public void Close()
+        {
+            if (_Commport != null && _Commport.PortOpened)
+            {
+                _Commport.Close();
+            }
+        }
+
+        public Packet Request(Packet p)
+        {
+            if (IsOpened)
+            {
+                lock (_DataLocker)
+                {
+                    _PacketReceived.Reset();
+                    _RecievedData.Clear();
+                    _Response = null;
+                }
+                _Commport.SendData(p.ToBinary());
+                _PacketReceived.WaitOne(2000);
+                return _Response;
+            }
+            return null;
+        }
+
+        public BusResult ReadValue(string address, out decimal v)
+        {
+            v = 0;
+            return BusResult.Fail;
+        }
+        #endregion
+    }
+}
--- Source/LJH.ZNCB.Device/DeviceBusService.cs
+++ Source/LJH.ZNCB.Device/DeviceBusService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO ;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,9 @@ namespace LJH.ZNCB.Device
                 return _Instance;
             }
         }
+
+        private static object _BusesLocker = new object();
+        private static List<DeviceBus> _Buses = new List<DeviceBus>();
         #endregion
 
         #region 构造函数
@@ -27,23 +31,44 @@ namespace LJH.ZNCB.Device
         #endregion
 
         #region 公共方法
+        private DeviceBus GetBus(byte comport)
+        {
+            lock (_BusesLocker)
+            {
+                var ret = _Buses.SingleOrDefault(it => it.Comport == comport);
+                if (ret == null)
+                {
+                    ret = new DeviceBus(comport, 1200);
+                    _Buses.Add(ret);
+                }
+                return ret;
+            }
+        }
         /// <summary>
         /// 读取某个设备的当前值
         /// </summary>
         /// <param name="address"></param>
         /// <param name="v"></param>
         /// <returns></returns>
-        public BusResult ReadValue(string address, out decimal v)
+        public BusResult ReadValue(byte comport, string address, out decimal v)
         {
             v = 0;
-            return BusResult.Fail;
+            var bus = GetBus(comport);
+            if (bus != null && bus.IsOpened)
+            {
+                return bus.ReadValue(address, out v);
+            }
+            else
+            {
+                return BusResult.Fail;
+            }
         }
         #endregion
     }
 
     public enum BusResult
     {
-        Success=0,
-        Fail=1
+        Success = 0,
+        Fail = 1
     }
 }
